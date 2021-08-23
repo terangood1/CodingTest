49ac762b161d01fbac1d493dedac58205a325e42
terangood1 <terangood1@naver.com>
2021-08-23 23:25:01 +0900

    commit

---
+++ list_7.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+test_case = int(stdin.readline())
+result = []
+
+
+for i in range(test_case):
+    a= list(map(int, stdin.readline().split()))
+    avg = sum(a[1:])/a[0]
+    
+
+
+
+    
