0c05dcb57153617e2c8ad6343d3459f2df4e7bbd
terangood1 <terangood1@naver.com>
2021-08-25 13:46:07 +0900

    commit

--- list_7.py
+++ list_7.py
@@ -4,10 +4,18 @@ test_case = int(stdin.readline())
 result = []
 
 
-for i in range(test_case):
+for i in range(test_case): # i는 0~ 케이스 수
     a= list(map(int, stdin.readline().split()))
-    avg = sum(a[1:])/a[0]
-    
+    avg = sum(a[1:])/a[0] # 첫번째 케이스 평균
+    count = 0
+    score = a[1:]
+    for k in score:
+        if avg<k:
+            count +=1
+    result.append((count/a[0])*100)
+
+for i in result:
+    print("{:.3f}%".format(i))
 
 
 
