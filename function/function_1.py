2263469d01a9628efdd7c75e0995d80292df0d34
terangood1 <terangood1@naver.com>
2021-08-27 22:11:42 +0900

    commit

---
+++ function/function_1.py
@@ -0,0 +1,8 @@
+k = [1,2,3,4]
+def solve(a):
+    result = 0
+    for i in a:
+        result += i
+    return result
+
+print(solve(k))
---
+++ function/function_2.py
@@ -0,0 +1,10 @@
+
+def solve(a):
+    k = int(a/10)
+    m = a%10
+    n = a+k+m
+    return n
+
+a = [i for i in range(100)]
+
+print(a)
