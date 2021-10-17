c3a0ec24fddbf3df833b3b424103694efbe45e23
terangood1 <terangood1@naver.com>
2021-10-17 23:37:52 +0900

    commit

--- 백준/math1_q/math2_2.py
+++ 백준/math1_q/math2_2.py
@@ -3,16 +3,16 @@ b=int(input())
 result=[]
 
 for i in range(a,b+1):
-    if i==1:
-        pass
-    elif i==2:
-        result.append(i)
-    else:
-        for j in range(2, i):
-            if i%j==0:
-                break
-            elif j==i-1:
-                result.append(i)
+    if i!=1:
+        
+        if i==2:
+            result.append(i)
+        else:
+            for j in range(2, i):
+                if i%j==0:
+                    break
+                elif j==i-1:
+                    result.append(i)
 if len(result)==0:
     print('-1')
 else:
