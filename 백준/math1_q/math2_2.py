646c5cb2bc5b0232bec4e06f98733f32f2b96cee
terangood1 <terangood1@naver.com>
2021-10-17 23:35:56 +0900

    commit

---
+++ 백준/math1_q/math2_2.py
@@ -0,0 +1,20 @@
+a=int(input())
+b=int(input())
+result=[]
+
+for i in range(a,b+1):
+    if i==1:
+        pass
+    elif i==2:
+        result.append(i)
+    else:
+        for j in range(2, i):
+            if i%j==0:
+                break
+            elif j==i-1:
+                result.append(i)
+if len(result)==0:
+    print('-1')
+else:
+    print(sum(result))
+    print(min(result))
