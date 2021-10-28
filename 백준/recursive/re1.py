897f02eb7ca428a6cc277079dbbb2a2aeb91718f
terangood1 <terangood1@naver.com>
2021-10-28 23:28:29 +0900

    commit

--- 백준/recursive/re1.py
+++ 백준/recursive/re1.py
@@ -1,9 +1,7 @@
 #0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
 
 def recursive(n):
-    if n == 0:
-        return 1
-    if n == 1:
+    if n <= 1:
         return 1
     else:
         return n*recursive(n-1)
